301b359512a9db7681e3fba39fa1577256a42bcc
mdconnecthelp <mdconnecthelp@gmail.com>
2025-07-01 17:02:46 -0400

    Update kbalookup.js

--- kbalookup.js
+++ kbalookup.js
@@ -2,7 +2,7 @@ var ticketSubjectID = 'attribute37';
 document.getElementById(ticketSubjectID).onblur = function() {
 $.get('../../Shared/AutocompleteSearch?componentStr=kb&searchText=' + $('#' + ticketSubjectID).val(),null,function(result) {
 if (result.length > 0){
-var html = result.map(item => "<a href=\"" + item.itemUrl + "\">" + item.title + "</a>").join("<br>");
+var html = result.map(item => "<a href=\"" + item.itemUrl + "\">" + item.title + "target=\"_blank\"</a>").join("<br>");
 html = "<div id=\"kbrelated\" class=\"p-2\" style=\"padding-bottom: 2em;\"><h3><b>Related Articles - Click for Additional Help About Your Ticket Subject <h3></b>" + html + "</h3></div>";
 // if it exists, replace it
 if ($("#kbrelated").length) {
